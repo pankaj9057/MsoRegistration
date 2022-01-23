a06dddd20e50a6e3099257927da6a9a2cc443f8f
pankaj9057 <84174032+pankaj9057@users.noreply.github.com>
2022-01-23 15:43:47 +0000

    Updates

---
+++ precache-manifest.abbb093ef88d0ffcb700be6670c68454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23fa35a68cd814cebdf2",
+    "url": "/MsoRegistration/static/css/main.6e407fa5.chunk.css"
+  },
+  {
+    "revision": "23fa35a68cd814cebdf2",
+    "url": "/MsoRegistration/static/js/main.23fa35a6.chunk.js"
+  },
+  {
+    "revision": "22d3e33e0659a9767853",
+    "url": "/MsoRegistration/static/js/1.22d3e33e.chunk.js"
+  },
+  {
+    "revision": "0d5b5e9177247933c5bc",
+    "url": "/MsoRegistration/static/js/2.0d5b5e91.chunk.js"
+  },
+  {
+    "revision": "5e3c2e57dc0726c46f5f",
+    "url": "/MsoRegistration/static/js/runtime~main.5e3c2e57.js"
+  },
+  {
+    "revision": "dd08f33ae34abe59c7fe9b77e398ca0d",
+    "url": "/MsoRegistration/static/media/main_logo_left.dd08f33a.png"
+  },
+  {
+    "revision": "6b9585106a810d575f755841f79df534",
+    "url": "/MsoRegistration/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MsoRegistration/precache-manifest.f2e1c4984caf21bdd8e5df367ce6569e.js"
+  "/MsoRegistration/precache-manifest.abbb093ef88d0ffcb700be6670c68454.js"
 );
 
 workbox.clientsClaim();
